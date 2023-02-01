43187c3f5f1e9cfd398341f62f145f682102979d
adridm97 <adri.d.m1997@gmail.com>
2023-02-01 18:42:58 +0100

    creación del proyecto ecommerce

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
-import '@/styles/globals.css'
-
+import "../scss/global.scss";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
